a36b26d178bc5e090e15eff8aafd807d740d2a3c
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-04-02 22:25:19 +0300

    RoomDb version = 2 (попытка сделать миграцию с версии в маркете)

--- app/src/main/java/sgtmelon/scriptum/app/room/RoomDb.kt
+++ app/src/main/java/sgtmelon/scriptum/app/room/RoomDb.kt
@@ -15,7 +15,7 @@ import sgtmelon.scriptum.app.room.dao.RollDao
 /**
  * Класс для общения с базой данных
  */
-@Database(entities = [NoteItem::class, RollItem::class, RankItem::class], version = 1)
+@Database(entities = [NoteItem::class, RollItem::class, RankItem::class], version = 2)
 abstract class RoomDb : RoomDatabase() {
 
     abstract fun getNoteDao(): NoteDao
