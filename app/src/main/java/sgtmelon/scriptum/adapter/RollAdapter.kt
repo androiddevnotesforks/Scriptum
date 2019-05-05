ccbc55b20fb04f085e536c4ac52cc686cdcd0a41
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-05-05 13:49:38 +0300

    Немного разобрал RollAdapter
    Tag: @Refactor

--- app/src/main/java/sgtmelon/scriptum/adapter/RollAdapter.kt
+++ app/src/main/java/sgtmelon/scriptum/adapter/RollAdapter.kt
@@ -7,8 +7,6 @@ import sgtmelon.scriptum.R
 import sgtmelon.scriptum.adapter.holder.RollReadHolder
 import sgtmelon.scriptum.adapter.holder.RollWriteHolder
 import sgtmelon.scriptum.control.input.InputCallback
-import sgtmelon.scriptum.databinding.ItemRollReadBinding
-import sgtmelon.scriptum.databinding.ItemRollWriteBinding
 import sgtmelon.scriptum.model.item.RollItem
 import sgtmelon.scriptum.model.state.NoteState
 import sgtmelon.scriptum.office.intf.ItemListener
@@ -34,19 +32,13 @@ class RollAdapter(context: Context,
     var cursorPosition = UNDEFINED
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType == read) {
-            true -> {
-                val binding: ItemRollReadBinding =
-                        inflater.inflateBinding(R.layout.item_roll_read, parent)
-
-                RollReadHolder(binding, clickListener)
-            }
-            false -> {
-                val binding: ItemRollWriteBinding =
-                        inflater.inflateBinding(R.layout.item_roll_write, parent)
-
-                RollWriteHolder(binding, dragListener, textChangeCallback, inputCallback)
-            }
+        return if (viewType == TYPE_READ) {
+            RollReadHolder(inflater.inflateBinding(R.layout.item_roll_read, parent), clickListener)
+        } else {
+            RollWriteHolder(
+                    inflater.inflateBinding(R.layout.item_roll_write, parent),
+                    dragListener, textChangeCallback, inputCallback
+            )
         }
     }
 
@@ -66,16 +58,13 @@ class RollAdapter(context: Context,
         }
     }
 
-    override fun getItemViewType(position: Int) = when (!noteState.isEdit) {
-        true -> read
-        false -> write
-    }
+    override fun getItemViewType(position: Int) = if (noteState.isEdit) TYPE_WRITE else TYPE_READ
 
     private companion object {
         private const val UNDEFINED = -1
 
-        private const val read = 0
-        private const val write = 1
+        private const val TYPE_WRITE = 0
+        private const val TYPE_READ = 1
     }
 
 }
