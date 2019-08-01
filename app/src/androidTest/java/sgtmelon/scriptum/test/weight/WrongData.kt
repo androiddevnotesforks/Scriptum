4b26be447a9589eebd3859e42f4cce18ff1313ff
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-08-01 22:10:00 +0300

    Add base for weight tests
    Tag: #Test #New

---
+++ app/src/androidTest/java/sgtmelon/scriptum/test/weight/WrongData.kt
@@ -0,0 +1,28 @@
+package sgtmelon.scriptum.test.weight
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import sgtmelon.scriptum.test.ParentUiTest
+
+/**
+ * Test open screens with wrong intent data, check error handling
+ *
+ * @author SerjantArbuz
+ */
+@RunWith(AndroidJUnit4::class)
+class WrongData : ParentUiTest() {
+
+    @Test fun bindOpen() {
+
+    }
+
+    @Test fun alarmOpen() {
+
+    }
+
+    @Test fun noteOpen() {
+
+    }
+
+}
