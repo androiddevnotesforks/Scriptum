d9e352b1c63988eb8cb9bbea299c85a50ba50418
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-09-16 09:17:25 +0300

    Remove from parentViewModel creation of repositories
    Tag: #Refactor

--- app/src/main/java/sgtmelon/scriptum/screen/vm/ParentViewModel.kt
+++ app/src/main/java/sgtmelon/scriptum/screen/vm/ParentViewModel.kt
@@ -4,12 +4,6 @@ import android.app.Application
 import android.content.Context
 import androidx.annotation.CallSuper
 import androidx.lifecycle.AndroidViewModel
-import sgtmelon.scriptum.repository.alarm.AlarmRepo
-import sgtmelon.scriptum.repository.alarm.IAlarmRepo
-import sgtmelon.scriptum.repository.preference.IPreferenceRepo
-import sgtmelon.scriptum.repository.preference.PreferenceRepo
-import sgtmelon.scriptum.repository.room.IRoomRepo
-import sgtmelon.scriptum.repository.room.RoomRepo
 import sgtmelon.scriptum.screen.vm.callback.IParentViewModel
 
 /**
@@ -18,10 +12,6 @@ import sgtmelon.scriptum.screen.vm.callback.IParentViewModel
 abstract class ParentViewModel<T>(application: Application) : AndroidViewModel(application) {
 
     protected val context: Context = application.applicationContext
-
-    protected val iPreferenceRepo: IPreferenceRepo = PreferenceRepo(context)
-    protected val iRoomRepo: IRoomRepo = RoomRepo(context)
-    protected val iAlarmRepo: IAlarmRepo = AlarmRepo(context)
 
     var callback: T? = null
 
