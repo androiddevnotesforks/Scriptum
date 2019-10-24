08b6485c706cabb30149966b6c459a880f9bd478
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-10-24 09:01:36 +0300

    Add full migration for alarmEntity index unique
    Tag: #Refactor

--- app/src/main/java/sgtmelon/scriptum/room/Migrate.kt
+++ app/src/main/java/sgtmelon/scriptum/room/Migrate.kt
@@ -42,6 +42,54 @@ object Migrate {
              */
             execSQL("DROP INDEX index_ROLL_TABLE_RL_NOTE_ID")
             execSQL("CREATE INDEX ROLL_TABLE_NOTE_ID_INDEX ON ROLL_TABLE(RL_NOTE_ID)")
+
+            /**
+             * Rank table:
+             *
+             * Expected:
+             * TableInfo{name='RANK_TABLE', columns={
+             * RK_NAME=Column{name='RK_NAME', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_VISIBLE=Column{name='RK_VISIBLE', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_POSITION=Column{name='RK_POSITION', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_NOTE_ID=Column{name='RK_NOTE_ID', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_ID=Column{name='RK_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=1, defaultValue='null'}},
+             * foreignKeys=[],
+             * indices=[Index{name='RANK_TABLE_NAME_INDEX', unique=true, columns=[RK_NAME]}]}
+             *
+             * Found:
+             * TableInfo{name='RANK_TABLE', columns={
+             * RK_VISIBLE=Column{name='RK_VISIBLE', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_NOTE_ID=Column{name='RK_NOTE_ID', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_ID=Column{name='RK_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=1, defaultValue='null'},
+             * RK_NAME=Column{name='RK_NAME', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * RK_POSITION=Column{name='RK_POSITION', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'}},
+             * foreignKeys=[],
+             * indices=[Index{name='index_RANK_TABLE_RK_NAME', unique=true, columns=[RK_NAME]}]}
+             */
+            execSQL("DROP INDEX index_RANK_TABLE_RK_NAME")
+            execSQL("CREATE UNIQUE INDEX RANK_TABLE_NAME_INDEX ON RANK_TABLE(RK_NAME)")
+
+            /**
+             * Alarm table:
+             *
+             * Expected:
+             * TableInfo{name='ALARM_TABLE', columns={
+             * AL_ID=Column{name='AL_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=1, defaultValue='null'},
+             * AL_DATE=Column{name='AL_DATE', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * AL_NOTE_ID=Column{name='AL_NOTE_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'}},
+             * foreignKeys=[ForeignKey{referenceTable='NOTE_TABLE', onDelete='CASCADE', onUpdate='CASCADE', columnNames=[AL_NOTE_ID], referenceColumnNames=[NT_ID]}],
+             * indices=[Index{name='ALARM_TABLE_NOTE_ID_INDEX', unique=true, columns=[AL_NOTE_ID]}]}
+             *
+             * Found:
+             * TableInfo{name='ALARM_TABLE', columns={
+             * AL_ID=Column{name='AL_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=1, defaultValue='null'},
+             * AL_DATE=Column{name='AL_DATE', type='TEXT', affinity='2', notNull=true, primaryKeyPosition=0, defaultValue='null'},
+             * AL_NOTE_ID=Column{name='AL_NOTE_ID', type='INTEGER', affinity='3', notNull=true, primaryKeyPosition=0, defaultValue='null'}},
+             * foreignKeys=[ForeignKey{referenceTable='NOTE_TABLE', onDelete='CASCADE', onUpdate='CASCADE', columnNames=[AL_NOTE_ID], referenceColumnNames=[NT_ID]}],
+             * indices=[Index{name='index_ALARM_TABLE_AL_NOTE_ID', unique=false, columns=[AL_NOTE_ID]}]}
+             */
+            execSQL("DROP INDEX index_ALARM_TABLE_AL_NOTE_ID")
+            execSQL("CREATE UNIQUE INDEX ALARM_TABLE_NOTE_ID_INDEX ON ALARM_TABLE(AL_NOTE_ID)")
         }
     }
 
@@ -203,7 +251,6 @@ object Migrate {
              * foreignKeys=[],
              * indices=[]}
              */
-
             execSQL("CREATE UNIQUE INDEX index_RANK_TABLE_RK_NAME ON RANK_TABLE(RK_NAME)")
         }
     }
