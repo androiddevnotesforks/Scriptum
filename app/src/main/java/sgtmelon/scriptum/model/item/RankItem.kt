9e40ec44765bafb8238c4790a7f58df0e4ab3440
SerjantArbuz <serjant.arbuz@yandex.ru>
2019-10-30 13:57:07 +0300

    Add rankItem
    Tag: #New

---
+++ app/src/main/java/sgtmelon/scriptum/model/item/RankItem.kt
@@ -0,0 +1,8 @@
+package sgtmelon.scriptum.model.item
+
+import sgtmelon.scriptum.adapter.RankAdapter
+
+/**
+ * Model for store short information about rank, use in [RankAdapter]
+ */
+class RankItem
