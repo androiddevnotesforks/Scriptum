f5834e1cf7d460c508a023b871e3417de09b9b45
SerjantArbuz <serjant.arbuz@yandex.ru>
2020-04-10 00:42:17 +0300

    Split InfoTest into 2 classes
    Tag: #Refactor #Test

---
+++ app/src/androidTest/java/sgtmelon/scriptum/test/control/notification/info/InfoCountTest.kt
@@ -0,0 +1,34 @@
+package sgtmelon.scriptum.test.control.notification.info
+
+import org.junit.Test
+import sgtmelon.scriptum.test.ParentNotificationTest
+
+/**
+ * Test for alarm notification count text.
+ */
+class InfoCountTest : ParentNotificationTest() {
+
+    /**
+     * Notify on start is implied
+     */
+
+    /**
+     * Will cancel notification
+     */
+    @Test fun displayInfoZero() = startDisplayTest(count = 0)
+
+    @Test fun displayInfoOne() = startDisplayTest(count = 1)
+
+    @Test fun displayInfoTwo() = startDisplayTest(count = 2)
+
+    @Test fun displayInfoFew() = startDisplayTest(count = 4)
+
+    @Test fun displayInfoMany() = startDisplayTest(count = 7)
+
+    private fun startDisplayTest(count: Int) {
+        repeat(count) { data.insertNotification() }
+
+        launch { onSee { mainScreen() } }
+    }
+
+}
--- app/src/androidTest/java/sgtmelon/scriptum/test/control/notification/info/InfoNotifyTest.kt
+++ app/src/androidTest/java/sgtmelon/scriptum/test/control/notification/info/InfoNotifyTest.kt
@@ -1,4 +1,4 @@
-package sgtmelon.scriptum.test.control.notification
+package sgtmelon.scriptum.test.control.notification.info
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
@@ -9,30 +9,11 @@ import sgtmelon.scriptum.test.ParentNotificationTest
  * Test of info about notification in status bar
  */
 @RunWith(AndroidJUnit4::class)
-class InfoTest : ParentNotificationTest() {
+class InfoNotifyTest : ParentNotificationTest() {
 
     /**
      * Notify on start is implied
      */
-
-    /**
-     * Will cancel notification
-     */
-    @Test fun displayInfoZero() = startDisplayTest(count = 0)
-
-    @Test fun displayInfoOne() = startDisplayTest(count = 1)
-
-    @Test fun displayInfoTwo() = startDisplayTest(count = 2)
-
-    @Test fun displayInfoFew() = startDisplayTest(count = 4)
-
-    @Test fun displayInfoMany() = startDisplayTest(count = 7)
-
-    private fun startDisplayTest(count: Int) {
-        repeat(count) { data.insertNotification() }
-
-        launch { onSee { mainScreen() } }
-    }
 
     /**
      * Notification in status bar should show - "You have 1 notification".
