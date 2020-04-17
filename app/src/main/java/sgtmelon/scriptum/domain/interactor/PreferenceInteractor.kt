de99fddae36162775f82efb7a919c1b63406b86a
SerjantArbuz <serjant.arbuz@yandex.ru>
2020-04-18 01:08:19 +0300

    Add I/PreferenceInteractor
    Tag: #New

---
+++ app/src/main/java/sgtmelon/scriptum/domain/interactor/PreferenceInteractor.kt
@@ -0,0 +1,10 @@
+package sgtmelon.scriptum.domain.interactor
+
+import sgtmelon.scriptum.presentation.screen.vm.callback.IPreferenceViewModel
+
+/**
+ * Interactor for [IPreferenceViewModel].
+ */
+class PreferenceInteractor {
+
+}
---
+++ app/src/main/java/sgtmelon/scriptum/domain/interactor/callback/IPreferenceInteractor.kt
@@ -0,0 +1,12 @@
+package sgtmelon.scriptum.domain.interactor.callback
+
+import sgtmelon.scriptum.domain.interactor.PreferenceInteractor
+
+/**
+ * Interface for [PreferenceInteractor].
+ */
+interface IPreferenceInteractor {
+
+
+
+}
