8b08734522eb4bc67d551a58e75ef5bd18f435b9
SerjantArbuz <serjant.arbuz@yandex.ru>
2021-03-30 01:13:14 +0300

    Add code inside getPreferenceList for PrintInteractor.kt

--- app/src/main/java/sgtmelon/scriptum/domain/interactor/impl/preference/PrintInteractor.kt
+++ app/src/main/java/sgtmelon/scriptum/domain/interactor/impl/preference/PrintInteractor.kt
@@ -1,11 +1,13 @@
 package sgtmelon.scriptum.domain.interactor.impl.preference
 
+import sgtmelon.scriptum.data.provider.PreferenceProvider
 import sgtmelon.scriptum.data.repository.preference.IPreferenceRepo
 import sgtmelon.scriptum.data.repository.room.callback.IDevelopRepo
 import sgtmelon.scriptum.domain.interactor.callback.preference.IPrintInteractor
 import sgtmelon.scriptum.domain.interactor.impl.ParentInteractor
 import sgtmelon.scriptum.domain.model.annotation.test.RunPrivate
 import sgtmelon.scriptum.domain.model.item.PrintItem
+import sgtmelon.scriptum.domain.model.item.PrintItem.Preference
 import sgtmelon.scriptum.domain.model.key.PrintType
 import sgtmelon.scriptum.presentation.screen.vm.callback.preference.IPrintViewModel
 
@@ -14,6 +16,8 @@ import sgtmelon.scriptum.presentation.screen.vm.callback.preference.IPrintViewMo
  */
 class PrintInteractor(
     private val developRepo: IDevelopRepo,
+    private val key: PreferenceProvider.Key,
+    private val def: PreferenceProvider.Def,
     private val preferenceRepo: IPreferenceRepo
 ) : ParentInteractor(),
     IPrintInteractor {
@@ -28,7 +32,30 @@ class PrintInteractor(
         PrintType.PREFERENCE -> getPreferenceList()
     }
 
+    // TODO add custom things (save path)
     @RunPrivate fun getPreferenceList(): List<PrintItem.Preference> {
-        return TODO()
+        return listOf(
+            Preference.Divider("TODO"),
+            Preference.Item(key.firstStart, def.firstStart, preferenceRepo.firstStart),
+            Preference.Item(key.theme, def.theme, preferenceRepo.theme),
+            Preference.Divider("TODO"),
+            Preference.Item(key.importSkip, def.importSkip, preferenceRepo.importSkip),
+            Preference.Divider("TODO"),
+            Preference.Item(key.sort, def.sort, preferenceRepo.sort),
+            Preference.Item(key.defaultColor, def.defaultColor, preferenceRepo.defaultColor),
+            Preference.Item(key.pauseSaveOn, def.pauseSaveOn, preferenceRepo.pauseSaveOn),
+            Preference.Item(key.autoSaveOn, def.autoSaveOn, preferenceRepo.autoSaveOn),
+            Preference.Item(key.savePeriod, def.savePeriod, preferenceRepo.savePeriod),
+            Preference.Divider("TODO"),
+            Preference.Item(key.repeat, def.repeat, preferenceRepo.repeat),
+            Preference.Item(key.signal, def.signal, preferenceRepo.signal),
+            Preference.Item(key.melodyUri, def.melodyUri, preferenceRepo.melodyUri),
+            Preference.Item(key.volume, def.volume, preferenceRepo.volume),
+            Preference.Item(key.volumeIncrease, def.volumeIncrease, preferenceRepo.volumeIncrease),
+            Preference.Divider("TODO"),
+            Preference.Item(key.isDeveloper, def.isDeveloper, preferenceRepo.isDeveloper),
+            Preference.Divider("TODO")
+        )
     }
+
 }
--- app/src/main/java/sgtmelon/scriptum/domain/model/item/PrintItem.kt
+++ app/src/main/java/sgtmelon/scriptum/domain/model/item/PrintItem.kt
@@ -7,10 +7,32 @@ import sgtmelon.scriptum.presentation.adapter.PrintAdapter
  * Model for store information about entities and preference keys, use in [PrintAdapter].
  */
 sealed class PrintItem {
+
     data class Note(val entity: NoteEntity) : PrintItem()
+
     data class Roll(val entity: RollEntity) : PrintItem()
+
     data class Visible(val entity: RollVisibleEntity) : PrintItem()
+
     data class Rank(val entity: RankEntity) : PrintItem()
+
     data class Alarm(val entity: AlarmEntity) : PrintItem()
-    data class Preference(val title: String, val value: String) : PrintItem()
+
+    sealed class Preference : PrintItem() {
+        data class Divider(val title: String) : Preference()
+
+        data class Item(val key: String, val def: String, val value: String) : Preference() {
+            constructor(
+                key: String,
+                def: Boolean,
+                value: Boolean
+            ) : this(key, def.toString(), value.toString())
+
+            constructor(
+                key: String,
+                def: Int,
+                value: Int
+            ) : this(key, def.toString(), value.toString())
+        }
+    }
 }
