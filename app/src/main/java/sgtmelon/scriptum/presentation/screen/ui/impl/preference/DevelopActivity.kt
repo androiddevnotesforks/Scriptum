19167650d940bce55968cd1fd6c82b47cf811ea5
SerjantArbuz <serjant.arbuz@yandex.ru>
2021-05-02 09:38:09 +0300

    Create ParentPreferenceActivity.kt and apply it

--- app/src/main/java/sgtmelon/scriptum/presentation/screen/ui/impl/preference/DevelopActivity.kt
+++ app/src/main/java/sgtmelon/scriptum/presentation/screen/ui/impl/preference/DevelopActivity.kt
@@ -2,78 +2,23 @@ package sgtmelon.scriptum.presentation.screen.ui.impl.preference
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.appcompat.widget.Toolbar
 import sgtmelon.scriptum.R
-import sgtmelon.scriptum.domain.model.annotation.Theme
-import sgtmelon.scriptum.extension.*
-import sgtmelon.scriptum.presentation.screen.ui.ScriptumApplication
-import sgtmelon.scriptum.presentation.screen.ui.impl.AppActivity
+import sgtmelon.scriptum.presentation.screen.ui.ParentPreferenceActivity
 
 /**
  * Screen for display [DevelopFragment].
  */
-class DevelopActivity : AppActivity() {
-
-    private val parentContainer by lazy { findViewById<ViewGroup>(R.id.develop_parent_container) }
-    private val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar_container) }
+class DevelopActivity : ParentPreferenceActivity(
+    R.layout.activity_develop,
+    R.id.develop_parent_container,
+    R.id.develop_fragment_container,
+    R.string.title_develop
+) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        ScriptumApplication.component.getAppBuilder().set(activity = this).build()
-            .inject(activity = this)
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_develop)
-
-        setupView()
-        setupInsets()
-
-        fm.beginTransaction()
-            .replace(R.id.develop_fragment_container, DevelopFragment())
-            .commit()
-    }
-
-    private fun setupView() {
-        toolbar?.apply {
-            title = getString(R.string.title_develop)
-            navigationIcon = getTintDrawable(R.drawable.ic_cancel_exit)
-            setNavigationOnClickListener { finish() }
-        }
-    }
-
-    private fun setupInsets() {
-        parentContainer?.doOnApplyWindowInsets { view, insets, _, _, margin ->
-            view.updateMargin(InsetsDir.LEFT, insets, margin)
-            view.updateMargin(InsetsDir.TOP, insets, margin)
-            view.updateMargin(InsetsDir.RIGHT, insets, margin)
-            return@doOnApplyWindowInsets insets
-        }
-    }
-
-    /**
-     * Make navigation translucent in portrait orientation.
-     */
-    override fun setNavigationColor(@Theme theme: Int) {
-        if (isPortraitMode()) {
-            window.navigationBarColor = getColorAttr(R.attr.clNavigationBar)
-        } else {
-            super.setNavigationColor(theme)
-        }
-    }
-
-    /**
-     * Make navigation translucent in portrait orientation.
-     */
-    @RequiresApi(Build.VERSION_CODES.P)
-    override fun setNavigationDividerColor(@Theme theme: Int) {
-        if (isPortraitMode()) {
-            window.navigationBarDividerColor = getColorAttr(R.attr.clNavigationBarDivider)
-        } else {
-            super.setNavigationDividerColor(theme)
-        }
+        showFragment(DevelopFragment())
     }
 
     companion object {
--- app/src/main/java/sgtmelon/scriptum/presentation/screen/ui/impl/preference/PreferenceActivity.kt
+++ app/src/main/java/sgtmelon/scriptum/presentation/screen/ui/impl/preference/PreferenceActivity.kt
@@ -2,78 +2,23 @@ package sgtmelon.scriptum.presentation.screen.ui.impl.preference
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.appcompat.widget.Toolbar
 import sgtmelon.scriptum.R
-import sgtmelon.scriptum.domain.model.annotation.Theme
-import sgtmelon.scriptum.extension.*
-import sgtmelon.scriptum.presentation.screen.ui.ScriptumApplication
-import sgtmelon.scriptum.presentation.screen.ui.impl.AppActivity
+import sgtmelon.scriptum.presentation.screen.ui.ParentPreferenceActivity
 
 /**
  * Screen for display [PreferenceFragment].
  */
-class PreferenceActivity : AppActivity() {
-
-    private val parentContainer by lazy { findViewById<ViewGroup>(R.id.preference_parent_container) }
-    private val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar_container) }
+class PreferenceActivity : ParentPreferenceActivity(
+    R.layout.activity_preference,
+    R.id.preference_parent_container,
+    R.id.preference_fragment_container,
+    R.string.title_preference
+) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        ScriptumApplication.component.getAppBuilder().set(activity = this).build()
-                .inject(activity = this)
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_preference)
-
-        setupView()
-        setupInsets()
-
-        fm.beginTransaction()
-                .replace(R.id.preference_fragment_container, PreferenceFragment())
-                .commit()
-    }
-
-    private fun setupView() {
-        toolbar?.apply {
-            title = getString(R.string.title_preference)
-            navigationIcon = getTintDrawable(R.drawable.ic_cancel_exit)
-            setNavigationOnClickListener { finish() }
-        }
-    }
-
-    private fun setupInsets() {
-        parentContainer?.doOnApplyWindowInsets { view, insets, _, _, margin ->
-            view.updateMargin(InsetsDir.LEFT, insets, margin)
-            view.updateMargin(InsetsDir.TOP, insets, margin)
-            view.updateMargin(InsetsDir.RIGHT, insets, margin)
-            return@doOnApplyWindowInsets insets
-        }
-    }
-
-    /**
-     * Make navigation translucent in portrait orientation.
-     */
-    override fun setNavigationColor(@Theme theme: Int) {
-        if (isPortraitMode()) {
-            window.navigationBarColor = getColorAttr(R.attr.clNavigationBar)
-        } else {
-            super.setNavigationColor(theme)
-        }
-    }
-
-    /**
-     * Make navigation translucent in portrait orientation.
-     */
-    @RequiresApi(Build.VERSION_CODES.P)
-    override fun setNavigationDividerColor(@Theme theme: Int) {
-        if (isPortraitMode()) {
-            window.navigationBarDividerColor = getColorAttr(R.attr.clNavigationBarDivider)
-        } else {
-            super.setNavigationDividerColor(theme)
-        }
+        showFragment(PreferenceFragment())
     }
 
     companion object {
