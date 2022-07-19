0125c8e99bfcef08f879c9bb6afa7f6b71a08f5e
SerjantArbuz <serjant.arbuz@yandex.ru>
2022-07-19 14:28:05 +0300

    Move summaries and arrays inside another xml

--- app/src/main/java/sgtmelon/scriptum/cleanup/presentation/provider/SummaryProvider.kt
+++ app/src/main/java/sgtmelon/scriptum/cleanup/presentation/provider/SummaryProvider.kt
@@ -3,23 +3,22 @@ package sgtmelon.scriptum.cleanup.presentation.provider
 
 import android.content.res.Resources
 import sgtmelon.scriptum.R
-import sgtmelon.scriptum.cleanup.presentation.screen.vm.impl.preference.PreferenceViewModel
 import sgtmelon.scriptum.infrastructure.model.key.Theme
 
 /**
- * Provider of summary arrays for [PreferenceViewModel]
+ * Provide summary/description for preference keys.
  */
 class SummaryProvider(private val resources: Resources) {
-
-    val sort: Array<String> = resources.getStringArray(R.array.pref_text_note_sort)
-    val color: Array<String> = resources.getStringArray(R.array.pref_text_note_color)
-    val savePeriod: Array<String> = resources.getStringArray(R.array.pref_text_note_save_period)
 
     fun getTheme(theme: Theme) = resources.getString(when (theme) {
         Theme.LIGHT -> R.string.pref_text_app_theme_light
         Theme.DARK -> R.string.pref_text_app_theme_dark
         Theme.SYSTEM -> R.string.pref_text_app_theme_system
     })
+
+    val sort: Array<String> = resources.getStringArray(R.array.pref_text_note_sort)
+    val color: Array<String> = resources.getStringArray(R.array.pref_text_note_color)
+    val savePeriod: Array<String> = resources.getStringArray(R.array.pref_text_note_save_period)
 
     val repeat: Array<String> = resources.getStringArray(R.array.pref_text_alarm_repeat)
     val signal: Array<String> = resources.getStringArray(R.array.pref_text_alarm_signal)
