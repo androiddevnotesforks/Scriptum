845248ad3fe24f3fd5f92c5787289f0a66ccd7e3
SerjantArbuz <serjant.arbuz@yandex.ru>
2022-08-03 00:34:31 +0300

    Tests for AlarmDataSourceImplTest.kt

--- app/src/test/java/sgtmelon/scriptum/infrastructure/database/dataSource/AlarmDataSourceImplTest.kt
+++ app/src/test/java/sgtmelon/scriptum/infrastructure/database/dataSource/AlarmDataSourceImplTest.kt
@@ -1,49 +1,189 @@
 package sgtmelon.scriptum.infrastructure.database.dataSource
 
+import io.mockk.coEvery
+import io.mockk.coVerifySequence
+import io.mockk.confirmVerified
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlin.random.Random
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
+import sgtmelon.scriptum.cleanup.data.room.entity.AlarmEntity
+import sgtmelon.scriptum.cleanup.domain.model.item.NotificationItem
+import sgtmelon.scriptum.cleanup.parent.ParentTest
+import sgtmelon.scriptum.infrastructure.database.dao.AlarmDao
+import sgtmelon.scriptum.infrastructure.database.dao.safe.AlarmDaoSafe
 
 /**
  * Test for [AlarmDataSourceImpl].
  */
-class AlarmDataSourceImplTest {
+class AlarmDataSourceImplTest : ParentTest(), AlarmDaoSafe {
+
+    @MockK lateinit var dao: AlarmDao
+
+    private val dataSource by lazy { AlarmDataSourceImpl(dao) }
+
+    override fun tearDown() {
+        super.tearDown()
+        confirmVerified(dao)
+    }
 
     @Test fun insert() {
-        TODO()
+        val entity = mockk<AlarmEntity>()
+        val id = Random.nextLong()
+
+        coEvery { dao.insert(entity) } returns id
+
+        runBlocking {
+            assertEquals(dataSource.insert(entity), id)
+        }
+
+        coVerifySequence {
+            dao.insert(entity)
+        }
     }
 
     @Test fun delete() {
-        TODO()
+        val noteId = Random.nextLong()
+
+        runBlocking {
+            dataSource.delete(noteId)
+        }
+
+        coVerifySequence {
+            dao.delete(noteId)
+        }
     }
 
     @Test fun update() {
-        TODO()
+        val entity = mockk<AlarmEntity>()
+
+        runBlocking {
+            dataSource.update(entity)
+        }
+
+        coVerifySequence {
+            dao.update(entity)
+        }
     }
 
-    @Test fun get() {
-        TODO()
+    @Test fun `get by noteId`() {
+        val id = mockk<Long>()
+        val entity = mockk<AlarmEntity>()
+
+        coEvery { dao.get(id) } returns null
+
+        runBlocking {
+            assertNull(dataSource.get(id))
+        }
+
+        coEvery { dao.get(id) } returns entity
+
+        runBlocking {
+            assertEquals(dataSource.get(id), entity)
+        }
+
+        coVerifySequence {
+            dao.get(id)
+            dao.get(id)
+        }
     }
 
-    @Test fun testGet() {
-        TODO()
+    @Test fun `get list`() {
+        val list = mockk<List<AlarmEntity>>()
+
+        coEvery { dao.get() } returns list
+
+        runBlocking {
+            assertEquals(dataSource.get(), list)
+        }
+
+        coVerifySequence {
+            dao.get()
+        }
     }
 
-    @Test fun testGet1() {
+    @Test fun `get by noteIdList`() {
         TODO()
+        //        val noteIdList = mockk<List<Long>>()
+        //        val list = mockk<List<AlarmEntity>>()
+        //
+        //        coEvery { dao.getSafe(noteIdList) } returns list
+        //
+        //        runBlocking {
+        //            assertEquals(dataSource.get(noteIdList), list)
+        //        }
+        //
+        //        coVerifySequence {
+        //            dao.getSafe(noteIdList)
+        //        }
     }
 
     @Test fun getItem() {
-        TODO()
+        val id = mockk<Long>()
+        val entity = mockk<AlarmEntity>()
+
+        coEvery { dao.get(id) } returns null
+
+        runBlocking {
+            assertNull(dataSource.get(id))
+        }
+
+        coEvery { dao.get(id) } returns entity
+
+        runBlocking {
+            assertEquals(dataSource.get(id), entity)
+        }
+
+        coVerifySequence {
+            dao.get(id)
+            dao.get(id)
+        }
     }
 
     @Test fun getItemList() {
-        TODO()
+        val list = mockk<List<NotificationItem>>()
+
+        coEvery { dao.getItemList() } returns list
+
+        runBlocking {
+            assertEquals(dataSource.getItemList(), list)
+        }
+
+        coVerifySequence {
+            dao.getItemList()
+        }
     }
 
     @Test fun getCount() {
-        TODO()
+        val count = Random.nextInt()
+
+        coEvery { dao.getCount() } returns count
+
+        runBlocking {
+            assertEquals(dataSource.getCount(), count)
+        }
+
+        coVerifySequence {
+            dao.getCount()
+        }
     }
 
-    @Test fun testGetCount() {
+    @Test fun `getCount by noteIdList`() {
         TODO()
+        //        val noteIdList = mockk<List<Long>>()
+        //        val count = Random.nextInt()
+        //
+        //        coEvery { dao.getCountSafe(noteIdList) } returns count
+        //
+        //        runBlocking {
+        //            assertEquals(dataSource.getCount(noteIdList), count)
+        //        }
+        //
+        //        coVerifySequence {
+        //            dao.getCountSafe(noteIdList)
+        //        }
     }
 }
