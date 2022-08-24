7d3f2e1e5766f5358c9d64d8d1666da38858b063
SerjantArbuz <serjant.arbuz@yandex.ru>
2022-08-25 01:52:04 +0300

    Test for insert in NoteDataSourceImplTest.kt

--- app/src/test/java/sgtmelon/scriptum/infrastructure/database/dataSource/NoteDataSourceImplTest.kt
+++ app/src/test/java/sgtmelon/scriptum/infrastructure/database/dataSource/NoteDataSourceImplTest.kt
@@ -1,10 +1,20 @@
 package sgtmelon.scriptum.infrastructure.database.dataSource
 
+import io.mockk.coEvery
+import io.mockk.coVerifySequence
 import io.mockk.confirmVerified
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlin.random.Random
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
+import sgtmelon.scriptum.cleanup.FastMock
+import sgtmelon.scriptum.cleanup.data.room.entity.NoteEntity
 import sgtmelon.scriptum.cleanup.parent.ParentTest
 import sgtmelon.scriptum.infrastructure.database.dao.NoteDao
+import sgtmelon.scriptum.infrastructure.database.dao.safe.insertSafe
 
 /**
  * Test for [NoteDataSourceImpl].
@@ -21,7 +31,26 @@ class NoteDataSourceImplTest : ParentTest() {
     }
 
     @Test fun insert() {
-        TODO()
+        val entity = mockk<NoteEntity>()
+        val id = Random.nextLong()
+
+        FastMock.Dao.noteDaoSafe()
+        coEvery { dao.insertSafe(entity) } returns null
+
+        runBlocking {
+            assertNull(dataSource.insert(entity))
+        }
+
+        coEvery { dao.insertSafe(entity) } returns id
+
+        runBlocking {
+            assertEquals(dataSource.insert(entity), id)
+        }
+
+        coVerifySequence {
+            dao.insertSafe(entity)
+            dao.insertSafe(entity)
+        }
     }
 
     @Test fun `delete by entity`() {
