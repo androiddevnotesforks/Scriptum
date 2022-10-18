f5f295b1e37dba5b4a272a47435f9598b19f060f
SerjantArbuz <lex.kuznet@gmail.com>
2022-10-18 11:28:14 +0300

    Start refactor NotePreferenceFragment.kt

---
+++ app/src/main/java/sgtmelon/scriptum/infrastructure/screen/preference/note/NotePreferenceDataBinding.kt
@@ -0,0 +1,18 @@
+package sgtmelon.scriptum.infrastructure.screen.preference.note
+
+import androidx.lifecycle.Lifecycle
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import sgtmelon.scriptum.R
+import sgtmelon.scriptum.infrastructure.screen.parent.ParentPreferenceBinding
+import sgtmelon.scriptum.infrastructure.utils.findPreference
+
+class NotePreferenceDataBinding(
+    lifecycle: Lifecycle,
+    fragment: PreferenceFragmentCompat?
+) : ParentPreferenceBinding(lifecycle, fragment) {
+
+    val sortButton: Preference? get() = fragment?.findPreference(R.string.pref_key_note_sort)
+    val colorButton: Preference? get() = fragment?.findPreference(R.string.pref_key_note_color)
+    val savePeriodButton: Preference? get() = fragment?.findPreference(R.string.pref_key_note_time)
+}
