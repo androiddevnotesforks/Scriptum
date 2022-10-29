9312c7cace64ca18f2e8f78a364e6d2031284202
SerjantArbuz <lex.kuznet@gmail.com>
2022-10-30 01:33:38 +0300

    Add BundleException.kt

--- app/src/main/java/sgtmelon/scriptum/infrastructure/screen/alarm/AlarmBundleProvider.kt
+++ app/src/main/java/sgtmelon/scriptum/infrastructure/screen/alarm/AlarmBundleProvider.kt
@@ -3,19 +3,23 @@ package sgtmelon.scriptum.infrastructure.screen.alarm
 import android.os.Bundle
 import sgtmelon.scriptum.infrastructure.model.data.IntentData.Note.Default
 import sgtmelon.scriptum.infrastructure.model.data.IntentData.Note.Intent
+import sgtmelon.scriptum.infrastructure.model.exception.BundleException
+import sgtmelon.scriptum.infrastructure.utils.record
 
 /**
  * Bundle data provider for [AlarmActivity] screen.
  */
 class AlarmBundleProvider {
 
-    // TODO record exception if noteId is null after getData
-
     private var _noteId: Long? = null
     val noteId: Long? get() = _noteId
 
     fun getData(bundle: Bundle?) {
         _noteId = bundle?.getLong(Intent.ID, Default.ID)?.takeIf { it != Default.ID }
+
+        if (noteId == null) {
+            BundleException(::noteId).record()
+        }
     }
 
     fun saveData(outState: Bundle) {
--- app/src/main/java/sgtmelon/scriptum/infrastructure/screen/preference/PreferenceBundleProvider.kt
+++ app/src/main/java/sgtmelon/scriptum/infrastructure/screen/preference/PreferenceBundleProvider.kt
@@ -3,14 +3,14 @@ package sgtmelon.scriptum.infrastructure.screen.preference
 import android.os.Bundle
 import sgtmelon.scriptum.infrastructure.model.data.IntentData.Preference.Default
 import sgtmelon.scriptum.infrastructure.model.data.IntentData.Preference.Intent
+import sgtmelon.scriptum.infrastructure.model.exception.BundleException
 import sgtmelon.scriptum.infrastructure.model.key.PreferenceScreen
+import sgtmelon.scriptum.infrastructure.utils.record
 
 /**
  * Bundle data provider for [PreferenceActivity] screen.
  */
 class PreferenceBundleProvider {
-
-    // TODO record exception if screen is null after getData
 
     private var _screen: PreferenceScreen? = null
     val screen: PreferenceScreen? get() = _screen
@@ -18,6 +18,10 @@ class PreferenceBundleProvider {
     fun getData(bundle: Bundle?) {
         val ordinal = bundle?.getInt(Intent.SCREEN, Default.SCREEN) ?: Default.SCREEN
         _screen = PreferenceScreen.values().getOrNull(ordinal)
+
+        if (screen == null) {
+            BundleException(::screen).record()
+        }
     }
 
     fun saveData(outState: Bundle) {
