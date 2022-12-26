ae4702e2a685a47fcd29a13d0bb8a59090df159e
SerjantArbuz <lex.kuznet@gmail.com>
2022-12-26 23:44:18 +0300

    Remove json converting from NotificationItem.kt, delete NotificationItemTest.kt

--- app/src/main/java/sgtmelon/scriptum/cleanup/domain/model/item/NotificationItem.kt
+++ app/src/main/java/sgtmelon/scriptum/cleanup/domain/model/item/NotificationItem.kt
@@ -3,7 +3,6 @@ package sgtmelon.scriptum.cleanup.domain.model.item
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.TypeConverters
-import org.json.JSONObject
 import sgtmelon.scriptum.infrastructure.adapter.NotificationAdapter
 import sgtmelon.scriptum.infrastructure.converter.key.ColorConverter
 import sgtmelon.scriptum.infrastructure.converter.key.NoteTypeConverter
@@ -29,35 +28,4 @@ data class NotificationItem(@Embedded val note: Note, @Embedded val alarm: Alarm
         @ColumnInfo(name = AlarmDb.ID) val id: Long,
         @ColumnInfo(name = AlarmDb.DATE) val date: String
     )
-
-    // TODO add converter toJson and back
-    fun toJson(): String = JSONObject().apply {
-        put(NoteDb.ID, note.id)
-        put(NoteDb.NAME, note.name)
-        put(NoteDb.COLOR, note.color.ordinal)
-        put(NoteDb.TYPE, note.type.ordinal)
-        put(AlarmDb.ID, alarm.id)
-        put(AlarmDb.DATE, alarm.date)
-    }.toString()
-
-    companion object {
-
-        // TODO add converter toJson and back
-        operator fun get(data: String): NotificationItem? = try {
-            val typeConverter = NoteTypeConverter()
-            val colorConverter = ColorConverter()
-
-            JSONObject(data).let {
-                val type = typeConverter.toEnum(it.getInt(NoteDb.TYPE)) ?: return@let null
-                val color = colorConverter.toEnum(it.getInt(NoteDb.COLOR)) ?: return@let null
-
-                val note = Note(it.getLong(NoteDb.ID), it.getString(NoteDb.NAME), color, type)
-                val alarm = Alarm(it.getLong(AlarmDb.ID), it.getString(AlarmDb.DATE))
-
-                return@let NotificationItem(note, alarm)
-            }
-        } catch (e: Throwable) {
-            null
-        }
-    }
 }
