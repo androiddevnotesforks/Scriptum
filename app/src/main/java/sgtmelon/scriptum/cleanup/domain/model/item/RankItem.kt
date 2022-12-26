2d026ab8c76f222e9a8ec164eeee0755d34ddcf9
SerjantArbuz <lex.kuznet@gmail.com>
2022-12-26 23:43:03 +0300

    Remove json converting from RankItem.kt, delete RankItemTest.kt

--- app/src/main/java/sgtmelon/scriptum/cleanup/domain/model/item/RankItem.kt
+++ app/src/main/java/sgtmelon/scriptum/cleanup/domain/model/item/RankItem.kt
@@ -2,8 +2,6 @@ package sgtmelon.scriptum.cleanup.domain.model.item
 
 import androidx.room.ColumnInfo
 import androidx.room.TypeConverters
-import org.json.JSONArray
-import org.json.JSONObject
 import sgtmelon.scriptum.infrastructure.adapter.RankAdapter
 import sgtmelon.scriptum.infrastructure.converter.types.BoolConverter
 import sgtmelon.scriptum.infrastructure.converter.types.NumbersJoinConverter
@@ -22,41 +20,4 @@ data class RankItem(
     @ColumnInfo(name = Rank.VISIBLE) var isVisible: Boolean = Default.VISIBLE,
     var bindCount: Int = Default.BIND_COUNT,
     var notificationCount: Int = Default.NOTIFICATION_COUNT
-) {
-
-    // TODO add converter toJson and back
-    fun toJson(): String = JSONObject().apply {
-        put(Rank.ID, id)
-        put(Rank.NOTE_ID, JSONArray().apply { for (item in noteId) put(item) })
-        put(Rank.POSITION, position)
-        put(Rank.NAME, name)
-        put(Rank.VISIBLE, isVisible)
-        put(Rank.BIND_COUNT, bindCount)
-        put(Rank.NOTIFICATION_COUNT, notificationCount)
-    }.toString()
-
-    companion object {
-        // TODO add converter toJson and back
-        operator fun get(data: String): RankItem? = try {
-            JSONObject(data).let {
-                val noteIdArray = it.getJSONArray(Rank.NOTE_ID)
-                val noteId = mutableListOf<Long>()
-                for (i in 0 until noteIdArray.length()) {
-                    noteId.add(noteIdArray.getLong(i))
-                }
-
-                return@let RankItem(
-                    it.getLong(Rank.ID),
-                    noteId,
-                    it.getInt(Rank.POSITION),
-                    it.getString(Rank.NAME),
-                    it.getBoolean(Rank.VISIBLE),
-                    it.getInt(Rank.BIND_COUNT),
-                    it.getInt(Rank.NOTIFICATION_COUNT)
-                )
-            }
-        } catch (e: Throwable) {
-            null
-        }
-    }
-}
+)
