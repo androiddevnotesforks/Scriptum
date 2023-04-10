1645e5d47c726d155a5fe9ac78afc4d69ca79da1
SerjantArbuz <lex.kuznet@gmail.com>
2023-04-10 17:55:08 +0300

    Fix ParentManualAdapter.kt work inside ListScreen.kt

--- app/src/main/java/sgtmelon/scriptum/infrastructure/screen/parent/list/ListScreen.kt
+++ app/src/main/java/sgtmelon/scriptum/infrastructure/screen/parent/list/ListScreen.kt
@@ -3,6 +3,8 @@ package sgtmelon.scriptum.infrastructure.screen.parent.list
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import sgtmelon.scriptum.infrastructure.adapter.parent.Adapter
+import sgtmelon.scriptum.infrastructure.adapter.parent.ParentListAdapter
+import sgtmelon.scriptum.infrastructure.adapter.parent.ParentManualAdapter
 import sgtmelon.scriptum.infrastructure.widgets.recycler.RecyclerInsertScroll
 
 /**
@@ -18,10 +20,17 @@ interface ListScreen<T> : Adapter.Manual.Callback,
     override val layoutManager: LinearLayoutManager
     override val recyclerView: RecyclerView?
 
+    /**
+     * Check first [Adapter.Manual] because [ParentManualAdapter] is instance of
+     * [ParentListAdapter] which is instance of [Adapter.Simple].
+     *
+     * So [ParentManualAdapter] will be checked as [Adapter.Simple] firstly. Instance not only of
+     * [Adapter.Manual].
+     */
     fun onListUpdate(list: List<T>) = adapter.let {
         when (it) {
-            is Adapter.Simple -> it.notifyList(list)
             is Adapter.Manual -> it.notifyList(list, viewModel.list.update, callback = this)
+            is Adapter.Simple -> it.notifyList(list)
         }
     }
 
