3f7e8600a54b040e6d0fa3d0d003343ba5644eb1
SerjantArbuz <lex.kuznet@gmail.com>
2023-07-17 00:03:25 +0200

    Tests for PermissionViewModelImplTest.kt

---
+++ app/src/test/java/sgtmelon/scriptum/infrastructure/screen/parent/permission/PermissionViewModelImplTest.kt
@@ -0,0 +1,54 @@
+package sgtmelon.scriptum.infrastructure.screen.parent.permission
+
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verifySequence
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import sgtmelon.scriptum.data.model.PermissionKey
+import sgtmelon.scriptum.data.repository.preferences.PreferencesRepo
+import sgtmelon.scriptum.testing.parent.ParentTest
+import sgtmelon.test.common.nextString
+import kotlin.random.Random
+
+/**
+ * Test for [PermissionViewModelImpl]
+ */
+class PermissionViewModelImplTest : ParentTest() {
+
+    @MockK lateinit var preferencesRepo: PreferencesRepo
+
+    private val viewModel by lazy { PermissionViewModelImpl(preferencesRepo) }
+
+    @After override fun tearDown() {
+        super.tearDown()
+        confirmVerified(preferencesRepo)
+    }
+
+    @Test fun isCalled() {
+        val key = PermissionKey(nextString())
+        val result = Random.nextBoolean()
+
+        every { preferencesRepo.isPermissionCalled(key) } returns result
+
+        assertEquals(viewModel.isCalled(key), result)
+
+        verifySequence {
+            preferencesRepo.isPermissionCalled(key)
+        }
+    }
+
+    @Test fun setCalled() {
+        val key = PermissionKey(nextString())
+
+        every { preferencesRepo.setPermissionCalled(key) } returns Unit
+
+        viewModel.setCalled(key)
+
+        verifySequence {
+            preferencesRepo.setPermissionCalled(key)
+        }
+    }
+}
