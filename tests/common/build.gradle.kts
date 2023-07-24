3c5c6b39c98cd07e48df247eb80eef03f37071dd
SerjantArbuz <lex.kuznet@gmail.com>
2023-07-24 03:27:43 +0200

    build.gradle -> build.gradle.kts for "common" module

---
+++ tests/common/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "sgtmelon.test.common"
+
+    compileSdk = AndroidConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = AndroidConfig.MIN_SDK
+        targetSdk = AndroidConfig.TARGET_SDK
+
+        consumerProguardFiles(ProFiles.CONSUMER)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile(ProFiles.DEFAULT), ProFiles.MAIN)
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(project(":libraries:extensions"))
+
+    implementation(libs.test.core)
+    implementation(libs.test.ext)
+}
