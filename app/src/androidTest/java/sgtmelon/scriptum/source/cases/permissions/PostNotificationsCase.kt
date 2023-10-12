26fe437e3e92b9723a1a440cb1d59b18785b8c2a
SerjantArbuz <lex.kuznet@gmail.com>
2023-10-12 16:39:26 +0200

    Add case description

--- app/src/androidTest/java/sgtmelon/scriptum/source/cases/permissions/PostNotificationsCase.kt
+++ app/src/androidTest/java/sgtmelon/scriptum/source/cases/permissions/PostNotificationsCase.kt
@@ -11,14 +11,19 @@ import sgtmelon.scriptum.source.ui.model.exception.ApiPermissionException
  */
 interface PostNotificationsCase {
 
+    /** User grant permission - feature available for use. */
     fun allow()
 
+    /** User deny permission, show information dialog + open settings. */
     fun denyInfo()
 
+    /** User deny permission, show information dialog + close. */
     fun denyInfoClose()
 
+    /** Same as [denyInfo] but with rotation. */
     fun denyInfoRotateWork()
 
+    /** Same as [denyInfoClose] but with rotation. */
     fun denyInfoRotateClose()
 
     fun throwOnLowApi() {
